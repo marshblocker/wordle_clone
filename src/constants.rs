39fc64bf5a799bcaa42ff1ab083788a0a3b0e9a6
Marshblocker <glmarinas@up.edu.ph>
2022-02-21 21:35:56 +0800

    Remove unneeded file path string literal constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,2 @@
 pub const MAX_GUESSES: usize = 5;
 pub const WORD_LENGTH: usize = 5;
-pub const UNKNOWN_WORDS_PATH: &str = "./src/unknown_words.txt";
-pub const ALLOWED_WORDS_PATH: &str = "./src/allowed_words.txt";
